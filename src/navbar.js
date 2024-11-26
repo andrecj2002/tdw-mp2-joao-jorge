f7fc9f009e1a7c5977262e7bb3dff3bffcbb7b57
andrecj2002 <kiko_411@outlook.com>
2024-11-26 17:06:51 +0000

    navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+
+  // Function to check if the link is active
+  const isActive = (path) => location.pathname === path ? "border-b-2 border-blue-500 text-blue-700" : "text-gray-700 dark:text-gray-400";
+
   return (
     <nav className="bg-transparent border-gray-200 px-6 sm:px-8 py-4 rounded dark:bg-gray-900">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -17,49 +22,40 @@ const Navbar = () => {
           className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
           id="navbar-search"
         >
-<ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-bold md:ml-auto">
-  <li>
-    <Link
-      to="/"
-      className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-    >
-      Home
-    </Link>
-  </li>
-  <li>
-    <Link
-      to="/armor-search"
-      className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-    >
-      Armor Search
-    </Link>
-  </li>
-  <li>
-    <Link
-      to="/monster-search"
-      className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-    >
-      Monster Search
-    </Link>
-  </li>
-  <li>
-    <Link
-      to="/profile"
-      className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-    >
-      Profile
-    </Link>
-  </li>
-  <li>
-    <Link
-      to="/auth"
-      className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-    >
-      Login/Register
-    </Link>
-  </li>
-</ul>
-
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-bold md:ml-auto">
+            <li>
+              <Link
+                to="/"
+                className={`block py-2 pr-4 pl-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 ${isActive("/")}`}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/armor-search"
+                className={`block py-2 pr-4 pl-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 ${isActive("/armor-search")}`}
+              >
+                Armor Search
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/monster-search"
+                className={`block py-2 pr-4 pl-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 ${isActive("/monster-search")}`}
+              >
+                Monster Search
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/weapons"
+                className={`block py-2 pr-4 pl-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 ${isActive("/weapons")}`}
+              >
+                Weapons
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>

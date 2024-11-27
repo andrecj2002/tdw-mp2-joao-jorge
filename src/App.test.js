061cdd69b57937dc02a6e71b6f19ef0f16a94620
andrecj2002 <joaoajorge@ua.ptm>
2024-11-27 09:34:21 +0000

    better testing, adapted navbar and pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import HomePage from './HomePage';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test("renders 'Hunter's Book' heading", () => {
+  render(<HomePage />);
+  const headingElement = screen.getByText(/Hunter's Book/i);
+  expect(headingElement).toBeInTheDocument();
 });
